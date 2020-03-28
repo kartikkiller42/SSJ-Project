e8d54f7c109958c5f6176ca50d8dfd63a501216a
kartik <kartik.tiwari93@gmail.com>
2020-03-29 01:21:18 +1100

    Character movement, LWRP, GlowyBoi Movement
    Character movement, LWRP, GlowyBoi Movement

---
+++ Assets/Scripts/GlowyBoiMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Script taken from https://answers.unity.com/questions/447663/rotate-object-around-other-object-or-circle-using.html
+public class GlowyBoiMovement : MonoBehaviour
+{
+    public Transform target;
+    public float fRadius = 1.0f;
+
+    void Update () 
+    {
+        Vector3 v3Pos = Camera.main.WorldToScreenPoint (target.position);
+        v3Pos = Input.mousePosition - v3Pos;
+        float angle = Mathf.Atan2 (v3Pos.y, v3Pos.x) * Mathf.Rad2Deg;
+        v3Pos = Quaternion.AngleAxis (angle, Vector3.forward) * (Vector3.right * fRadius);
+        transform.position = target.position + v3Pos;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Tutorial followed: https://www.youtube.com/watch?v=dwcT-Dch0bA&t=1012s
+public class PlayerMovement2 : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    private float horizontalMove = 0f;
+
+    public float runSpeed = 40f;
+
+    private bool jump = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw(("Horizontal"))*runSpeed;
+        if (Input.GetButtonDown(("Jump")))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove*Time.fixedDeltaTime,false,jump);
+        jump = false;
+    }
+}
